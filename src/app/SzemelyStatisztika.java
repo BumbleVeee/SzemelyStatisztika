70f44055f2d457c768b087dcfd249ae02a24183a
gubek.veronika <gubek.veronika@DESKTOP-6SEMLRJ.akk.lan>
2026-09-10 15:03:03 +0200

    szemlyi statisztika

---
+++ src/app/SzemelyStatisztika.java
@@ -0,0 +1,55 @@
+package app;
+
+public class SzemelyStatisztika {
+
+    public static void main(String[] args) {
+        kiiratas();
+    }
+
+    private static void kiiratas() {
+        String nev = "Bernadette";
+        int szulEv = 1964;
+        int aktualisEv = 2026;
+        int eletEv = eletkor(szulEv,aktualisEv);
+        int nyugdij = 65;
+        int hatralevoEv = nyugdijEv(eletEv, nyugdij);
+        
+        if (!megfeleloEletEv(eletEv)){
+            throw new IllegalArgumentException("Az élet év nem lehet kisebb mint 0!");
+        }
+        
+        if (!marNyugdijas(eletEv,nyugdij)){
+            throw new IllegalArgumentException("Már nyugdíjas vagy!");
+        }
+        
+        System.out.println(koszontes(nev));
+        System.out.println("Életkor: " + eletEv);
+        System.out.println("Nyugdíjig hátralévő évek száma: " + hatralevoEv);
+        
+        /* metódusok:
+        köszöntés
+        életkor: 2026 -
+        nyugdíjig hátrelévő évek száma: 65
+        */
+    }
+
+    private static String koszontes(String nev) {
+        return "Hello %s!".formatted(nev);
+    }
+
+    private static int eletkor(int szulEv, int aktualisEv) {
+        return aktualisEv-szulEv;
+    }
+
+    private static int nyugdijEv(int eletEv, int nyugdij) {
+        return nyugdij-eletEv;
+    }
+    
+    private static boolean megfeleloEletEv(int eletEv){
+        return eletEv<=0;
+    }
+    
+    private static boolean marNyugdijas(int eletEv, int nyugdij){
+        return eletEv<nyugdij;
+    }
+}
